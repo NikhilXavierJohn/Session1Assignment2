cb2f5f34b933d24acfb0209bba42e2a2efb0c910
NikhilXavierJohn <nkh.john@gmail.com>
2016-06-10 15:52:39 +0000

    Add files via upload

---
+++ Variabledeclaration.java
@@ -0,0 +1,13 @@
+
+public class Variabledeclaration {
+
+	@SuppressWarnings("unused")
+	public static void variabledeclration(){
+		int a,b,c;
+		b=4;
+		System.out.println(b);
+	}
+	public static void main(String[] args) {
+		variabledeclration();
+	}
+}
